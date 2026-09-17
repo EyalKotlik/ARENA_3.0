dd94d9c3b85506cc93241d3f36ccd0ce7259eb82
Eyal Kotlik <eyal.ktlk@proton.me>
2026-09-17 11:15:07 +0300

    C0.3

--- chapter0_fundamentals/exercises/part3_optimization/answers.py
+++ chapter0_fundamentals/exercises/part3_optimization/answers.py
@@ -296,6 +296,8 @@ class AdamW:
 
 
 tests.test_adamw(AdamW)
+
+
 # %%
 def opt_fn(
     fn: Callable,
@@ -313,7 +315,9 @@ def opt_fn(
 
     optimizer = optimizer_class([xy], **optimizer_hyperparams)
 
-    xy_list = [xy.detach().clone()]  # so that we don't unintentionally modify past values in `xy_list`
+    xy_list = [
+        xy.detach().clone()
+    ]  # so that we don't unintentionally modify past values in `xy_list`
 
     for i in range(n_iters):
         fn(xy[0], xy[1]).backward()
@@ -344,6 +348,8 @@ for optimizer_class, params in optimizer_list:
     points.append((xys, optimizer_class, params))
 
 plot_fn_with_points(pathological_curve_loss, min_points=[(0, "y_min")], points=points)
+
+
 # %%
 def bivariate_gaussian(x, y, x_mean=0.0, y_mean=0.0, x_sig=1.0, y_sig=1.0):
     norm = 1 / (2 * np.pi * x_sig * y_sig)
@@ -361,13 +367,21 @@ def neg_trimodal_func(x, y):
     non-adaptive methods can fail to converge to them in the first place given how shallow the
     gradients are everywhere except in the close vicinity of the minima.
     """
-    z = -bivariate_gaussian(x, y, x_mean=means[0][0], y_mean=means[0][1], x_sig=0.2, y_sig=0.2)
-    z -= bivariate_gaussian(x, y, x_mean=means[1][0], y_mean=means[1][1], x_sig=0.2, y_sig=0.2)
-    z -= bivariate_gaussian(x, y, x_mean=means[2][0], y_mean=means[2][1], x_sig=0.2, y_sig=0.2)
+    z = -bivariate_gaussian(
+        x, y, x_mean=means[0][0], y_mean=means[0][1], x_sig=0.2, y_sig=0.2
+    )
+    z -= bivariate_gaussian(
+        x, y, x_mean=means[1][0], y_mean=means[1][1], x_sig=0.2, y_sig=0.2
+    )
+    z -= bivariate_gaussian(
+        x, y, x_mean=means[2][0], y_mean=means[2][1], x_sig=0.2, y_sig=0.2
+    )
     return z
 
 
 plot_fn(neg_trimodal_func, x_range=(-2, 2), y_range=(-2, 2), min_points=means)
+
+
 # %%
 def rosenbrocks_banana_func(x: Tensor, y: Tensor, a=1, b=100) -> Tensor:
     """
@@ -388,11 +402,17 @@ plot_fn(
     z_range=(0, 100),
     min_points=[(1, 1)],
 )
+
+
 # %%
 def get_cifar() -> tuple[datasets.CIFAR10, datasets.CIFAR10]:
     """Returns CIFAR-10 train and test sets."""
-    cifar_trainset = datasets.CIFAR10(exercises_dir / "data", train=True, download=True, transform=IMAGENET_TRANSFORM)
-    cifar_testset = datasets.CIFAR10(exercises_dir / "data", train=False, download=True, transform=IMAGENET_TRANSFORM)
+    cifar_trainset = datasets.CIFAR10(
+        exercises_dir / "data", train=True, download=True, transform=IMAGENET_TRANSFORM
+    )
+    cifar_testset = datasets.CIFAR10(
+        exercises_dir / "data", train=False, download=True, transform=IMAGENET_TRANSFORM
+    )
     return cifar_trainset, cifar_testset
 
 
@@ -420,3 +440,292 @@ imshow(
     height=600,
     width=1000,
 )
+
+
+# %%
+@dataclass
+class ResNetFinetuningArgs:
+    n_classes: int = 10
+    batch_size: int = 128
+    epochs: int = 3
+    learning_rate: float = 1e-3
+    weight_decay: float = 0.0
+
+
+class ResNetFinetuner:
+    def __init__(self, args: ResNetFinetuningArgs):
+        self.args = args
+
+    def pre_training_setup(self):
+        self.model = get_resnet_for_feature_extraction(self.args.n_classes).to(device)
+        self.optimizer = AdamW(
+            self.model.out_layers[-1].parameters(),
+            lr=self.args.learning_rate,
+            weight_decay=self.args.weight_decay,
+        )
+        self.trainset, self.testset = get_cifar()
+        self.train_loader = DataLoader(
+            self.trainset, batch_size=self.args.batch_size, shuffle=True
+        )
+        self.test_loader = DataLoader(
+            self.testset, batch_size=self.args.batch_size, shuffle=False
+        )
+        self.logged_variables = {"loss": [], "accuracy": []}
+        self.examples_seen = 0
+
+    def training_step(
+        self,
+        imgs: Float[Tensor, "batch channels height width"],
+        labels: Int[Tensor, " batch"],
+    ) -> Float[Tensor, ""]:
+        """Perform a gradient update step on a single batch of data."""
+        imgs, labels = imgs.to(device), labels.to(device)
+
+        logits = self.model(imgs)
+        loss = F.cross_entropy(logits, labels)
+        loss.backward()
+        self.optimizer.step()
+        self.optimizer.zero_grad()
+
+        self.examples_seen += imgs.shape[0]
+        self.logged_variables["loss"].append(loss.item())
+        return loss
+
+    @t.inference_mode()
+    def evaluate(self) -> float:
+        """Evaluate the model on the test set and return the accuracy."""
+        self.model.eval()
+        total_correct, total_samples = 0, 0
+
+        for imgs, labels in tqdm(self.test_loader, desc="Evaluating"):
+            imgs, labels = imgs.to(device), labels.to(device)
+            logits = self.model(imgs)
+            total_correct += (logits.argmax(dim=1) == labels).sum().item()
+            total_samples += len(imgs)
+
+        accuracy = total_correct / total_samples
+        self.logged_variables["accuracy"].append(accuracy)
+        return accuracy
+
+    def train(self) -> dict[str, list[float]]:
+        self.pre_training_setup()
+
+        accuracy = self.evaluate()
+
+        for epoch in range(self.args.epochs):
+            self.model.train()
+
+            pbar = tqdm(self.train_loader, desc="Training")
+            for imgs, labels in pbar:
+                loss = self.training_step(imgs, labels)
+                pbar.set_postfix(loss=f"{loss:.3f}", ex_seen=f"{self.examples_seen:06}")
+
+            accuracy = self.evaluate()
+            pbar.set_postfix(
+                loss=f"{loss:.3f}",
+                accuracy=f"{accuracy:.2f}",
+                ex_seen=f"{self.examples_seen:06}",
+            )
+
+        return self.logged_variables
+
+
+# %%
+args = ResNetFinetuningArgs()
+trainer = ResNetFinetuner(args)
+logged_variables = trainer.train()
+
+
+line(
+    y=[logged_variables["loss"][: 391 * 3 + 1], logged_variables["accuracy"][:4]],
+    x_max=len(logged_variables["loss"][: 391 * 3 + 1] * args.batch_size),
+    yaxis2_range=[0, 1],
+    use_secondary_yaxis=True,
+    labels={"x": "Examples seen", "y1": "Cross entropy loss", "y2": "Test Accuracy"},
+    title="Feature extraction with ResNet34",
+    width=800,
+)
+
+
+# %%
+def test_resnet_on_random_input(
+    model: ResNet34, n_inputs: int = 3, seed: int | None = 42
+):
+    if seed is not None:
+        np.random.seed(seed)
+    indices = np.random.choice(len(cifar_trainset), n_inputs).tolist()
+    classes = [cifar_trainset.classes[cifar_trainset.targets[i]] for i in indices]
+    imgs = cifar_trainset.data[indices]
+    device = next(model.parameters()).device
+    with t.inference_mode():
+        x = t.stack(list(map(IMAGENET_TRANSFORM, imgs)))
+        logits: Tensor = model(x.to(device))
+    probs = logits.softmax(-1)
+    if probs.ndim == 1:
+        probs = probs.unsqueeze(0)
+    for img, label, prob in zip(imgs, classes, probs):
+        display(HTML(f"<h2>Classification probabilities (true class = {label})</h2>"))
+        imshow(
+            img,
+            width=200,
+            height=200,
+            margin=0,
+            xaxis_visible=False,
+            yaxis_visible=False,
+        )
+        bar(
+            prob,
+            x=cifar_trainset.classes,
+            width=600,
+            height=400,
+            text_auto=".2f",
+            labels={"x": "Class", "y": "Prob"},
+        )
+
+
+test_resnet_on_random_input(trainer.model)
+
+
+# %%
+@dataclass
+class WandbResNetFinetuningArgs(ResNetFinetuningArgs):
+    """Contains new params for use in wandb.init, as well as all the ResNetFinetuningArgs params."""
+
+    wandb_project: str | None = "arena-day3-resnet"
+    wandb_name: str | None = None
+
+
+class WandbResNetFinetuner(ResNetFinetuner):
+    args: WandbResNetFinetuningArgs  # adding this line helps with typechecker!
+    examples_seen: int = 0  # tracking examples seen (used as step for wandb)
+
+    def pre_training_setup(self):
+        """Initializes the wandb run using `wandb.init` and `wandb.watch`."""
+        super().pre_training_setup()
+        wandb.init(
+            project=self.args.wandb_project, name=self.args.wandb_name, config=self.args
+        )
+        wandb.watch(self.model.out_layers[-1], log="all", log_freq=50)
+
+    def training_step(
+        self,
+        imgs: Float[Tensor, "batch channels height width"],
+        labels: Int[Tensor, " batch"],
+    ) -> Float[Tensor, ""]:
+        """Equivalent to ResNetFinetuner.training_step, but logging the loss to wandb."""
+        imgs, labels = imgs.to(device), labels.to(device)
+
+        logits = self.model(imgs)
+        loss = F.cross_entropy(logits, labels)
+        loss.backward()
+        self.optimizer.step()
+        self.optimizer.zero_grad()
+
+        self.examples_seen += imgs.shape[0]
+        self.logged_variables["loss"].append(loss.item())
+        wandb.log({"loss": loss}, step=self.examples_seen)
+        return loss
+
+    @t.inference_mode()
+    def evaluate(self) -> float:
+        """Equivalent to ResNetFinetuner.evaluate, but logging the accuracy to wandb."""
+        self.model.eval()
+        total_correct, total_samples = 0, 0
+
+        for imgs, labels in tqdm(self.test_loader, desc="Evaluating"):
+            imgs, labels = imgs.to(device), labels.to(device)
+            logits = self.model(imgs)
+            total_correct += (logits.argmax(dim=1) == labels).sum().item()
+            total_samples += len(imgs)
+
+        accuracy = total_correct / total_samples
+        self.logged_variables["accuracy"].append(accuracy)
+        wandb.log({"accuracy": accuracy}, step=self.examples_seen)
+        return accuracy
+
+    def train(self) -> None:
+        """Equivalent to ResNetFinetuner.train, but with wandb integration."""
+        self.pre_training_setup()
+
+        accuracy = self.evaluate()
+
+        for epoch in range(self.args.epochs):
+            self.model.train()
+
+            pbar = tqdm(self.train_loader, desc="Training")
+            for imgs, labels in pbar:
+                loss = self.training_step(imgs, labels)
+                pbar.set_postfix(loss=f"{loss:.3f}", ex_seen=f"{self.examples_seen:06}")
+
+            accuracy = self.evaluate()
+            pbar.set_postfix(
+                loss=f"{loss:.3f}",
+                accuracy=f"{accuracy:.2f}",
+                ex_seen=f"{self.examples_seen:06}",
+            )
+        wandb.finish()
+
+
+args = WandbResNetFinetuningArgs()
+trainer = WandbResNetFinetuner(args)
+trainer.train()
+# %%
+# YOUR CODE HERE - fill `sweep_config` so it has the requested behaviour
+sweep_config = dict(
+    method="random",
+    metric=dict(name="accuracy", goal="maximize"),
+    parameters=dict(
+        lr=dict(min=1e-4, max=1e-1, distribution="log_uniform_values"),
+        batch_size=dict(values=[32, 64, 128, 256]),
+        weight_decay_active=dict(values=[0, 1]),
+        weight_decay=dict(min=1e-4, max=1e-2, distribution="log_uniform_values"),
+    ),
+)
+
+
+def update_args(
+    args: WandbResNetFinetuningArgs, sampled_parameters: dict
+) -> WandbResNetFinetuningArgs:
+    """
+    Returns a new args object with modified values. The dictionary `sampled_parameters` will have
+    the same keys as your `sweep_config["parameters"]` dict, and values equal to the sampled values
+    of those hyperparameters.
+    """
+    assert set(sampled_parameters.keys()) == set(sweep_config["parameters"].keys())
+
+    # YOUR CODE HERE - update `args` based on `sampled_parameters`
+    new_args = WandbResNetFinetuningArgs(
+        args.n_classes,
+        sampled_parameters["batch_size"],
+        args.epochs,
+        sampled_parameters["lr"],
+        (
+            sampled_parameters["weight_decay"]
+            if sampled_parameters["weight_decay_active"] == 1
+            else 0
+        ),
+        args.wandb_project,
+        args.wandb_name,
+    )
+    return new_args
+
+
+tests.test_sweep_config(sweep_config)
+tests.test_update_args(update_args, sweep_config)
+# %%
+def train():
+    # Define args & initialize wandb
+    args = WandbResNetFinetuningArgs()
+    wandb.init(project=args.wandb_project, name=args.wandb_name, reinit=False)
+
+    # After initializing wandb, we can update args using `wandb.config`
+    args = update_args(args, dict(wandb.config))
+
+    # Train the model with these new hyperparameters (the second `wandb.init` call will be ignored)
+    trainer = WandbResNetFinetuner(args)
+    trainer.train()
+
+
+sweep_id = wandb.sweep(sweep=sweep_config, project="arena-day3-resnet-sweep")
+wandb.agent(sweep_id=sweep_id, function=train, count=3)
+wandb.finish()
